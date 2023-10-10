360e57931e133e8be798abc379a909af59c56db2
Corintho Assuncao <corintho.rodrigues.de.assuncao@nn-group.com>
2023-10-10 19:33:42 +0200

    chore: add some QoL mappings

--- mappings.lua
+++ mappings.lua
@@ -1,6 +1,16 @@
 ---@type MappingsTable
 local M = {}
 
+M.general = {
+  n = {
+    ["<leader>qq"] = {"<cmd>qa<cr>", "Quit all"},
+    ["<leader>qQ"] = {"<cmd>qa!<cr>", "Force quit all"}
+  },
+  i = {
+    ["kj"] = { "<ESC>", "escape insert mode" , opts = { nowait = true }},
+  }
+}
+
 M.lazygit = {
   plugin = true,
   n = {
