9b8923531e46790ef314f5f83d06f6fc92a0c73f
Corintho Assuncao <corintho.rodrigues.de.assuncao@nn-group.com>
2023-10-19 11:15:04 +0200

    fix: better handling of highlights

--- chadrc.lua
+++ chadrc.lua
@@ -1,20 +1,17 @@
 ---@class ChadrcConfig
 local M = {}
 
+local highlights = require("custom.highlights")
 M.ui = {
   theme = "gatekeeper",
   statusline = {
     theme = "vscode_colored",
   },
-  hl_add = {
-    LazyGitBorder = {
-      fg = "black2",
-      bg = "black2",
-    },
-  },
   extended_integrations = {
     "trouble",
   },
+  hl_override = highlights.override,
+  hl_add = highlights.add,
 }
 
 M.plugins = "custom.plugins"
