003cff8dffc920ebb83c993c65cbdd7e51572331
Corintho Assuncao <corintho.rodrigues.de.assuncao@nn-group.com>
2023-10-20 11:29:31 +0200

    feat: add markdown support

--- plugins.lua
+++ plugins.lua
@@ -198,6 +198,16 @@ local plugins = {
       })
     end,
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
+  },
 }
 
 return plugins
