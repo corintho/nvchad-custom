f9806be9d3af8ec40a7457700ea0f45270538ddb
Corintho Assuncao <corintho.filho@gmail.com>
2023-10-21 16:33:24 +0200

    fix: proper setup for right hand sided nvimtree

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 vim.o.relativenumber = true
 vim.g.format_on_save = true
+vim.g.nvimtree_side = "right"
