ceef18df7c3d794db2470d351f27f323221144c0
Corintho Assuncao <corintho.rodrigues.de.assuncao@nn-group.com>
2023-10-22 19:18:47 +0200

    fix: disable format on save by default, since it breaks typescript formatting

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 vim.o.relativenumber = true
-vim.g.format_on_save = true
+vim.g.format_on_save = false
 vim.g.nvimtree_side = "right"
