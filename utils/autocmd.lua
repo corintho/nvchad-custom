b6bb8e33bd48ab1295488536c1dfc4f623253e96
Corintho Assunção <corintho.filho@gmail.com>
2024-04-10 13:22:08 +0200

    feat: enable inlay hints on neovim 0.10

--- utils/autocmd.lua
+++ utils/autocmd.lua
@@ -8,5 +8,20 @@ autocmd({ "BufNewFile", "BufRead" }, {
 
 autocmd({ "UIEnter" }, {
   desc = "Workaround to properly set theme colors",
-  callback = function() dofile(vim.g.base46_cache .. "syntax") end,
+  callback = function()
+    dofile(vim.g.base46_cache .. "syntax")
+  end,
 })
+
+if vim.fn.has("nvim-0.10") == 1 then
+  autocmd({ "LspAttach" }, {
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(args)
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      if client and client.server_capabilities.inlayHintProvider then
+        vim.lsp.inlay_hint(args.buf, true)
+      end
+      -- whatever other lsp config you want
+    end,
+  })
+end
