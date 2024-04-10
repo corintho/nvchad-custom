cd919fd2f8c0a6160f51d7b1ebaae34f6bdcf586
Corintho Assunção <corintho.filho@gmail.com>
2024-04-10 13:22:07 +0200

    feat: enable clipboard copying in windows WSL

--- init.lua
+++ init.lua
@@ -2,3 +2,18 @@ vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,
 vim.o.relativenumber = true
 vim.g.format_on_save = false
 vim.g.nvimtree_side = "right"
+
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = 'WslClipboard',
+    copy = {
+       ['+'] = 'clip.exe',
+       ['*'] = 'clip.exe',
+     },
+    paste = {
+       ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+       ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
