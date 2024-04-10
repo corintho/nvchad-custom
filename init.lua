b563e96239dbf81fe7d038736d244631dcee0114
Corintho Assunção <corintho.filho@gmail.com>
2024-04-10 13:22:07 +0200

    feat: add support to neovide

--- init.lua
+++ init.lua
@@ -3,6 +3,13 @@ vim.o.relativenumber = true
 vim.g.format_on_save = false
 vim.g.nvimtree_side = "right"
 
+-- Setup for neovide
+if vim.g.neovide then
+    vim.o.guifont = "JetBrainsMono Nerd Font Mono:h14"
+    vim.g.neovide_cursor_animation_length = 0
+end
+
+-- Setup clipboard for WSL
 if vim.fn.has("wsl") == 1 then
   vim.g.clipboard = {
     name = 'WslClipboard',
