dd92daa578008bff487a2e113c04cb94175356e4
Corintho Assunção <corintho.filho@gmail.com>
2024-04-22 11:25:57 +0200

    chore: clenaup old files

--- configs/conform.lua
+++
@@ -1,9 +0,0 @@
-return function(opts)
-  vim.api.nvim_create_user_command("ToggleFormatOnSave", function()
-    vim.g.format_on_save = not vim.g.format_on_save
-  end, {
-    desc = "Toggle format on save",
-  })
-  require("core.utils").load_mappings("format")
-  require("conform").setup(opts)
-end
